37efdc88c33af92f845a74cc54edb4d68c782477
sodii <minself@gmail.com>
2025-06-01 08:05:28 +0900

    fix

--- src/prim/unix/prim.c
+++ src/prim/unix/prim.c
@@ -31,10 +31,7 @@ terms of the MIT license. A copy of the license can be found in the file
 
 #if defined(__linux__)
   #include <features.h>
-  #include <linux/prctl.h>  // PR_SET_VMA
-  //#if defined(MI_NO_THP)
-  //#include <sys/prctl.h>    // THP disable
-  //#endif
+  #include <sys/prctl.h>    // THP disable
   #if defined(__GLIBC__)
   #include <linux/mman.h>   // linux mmap flags
   #else
